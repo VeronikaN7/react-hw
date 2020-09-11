6893c477e7b5d3bfdd1a4a0823f170a72ca61668
Veronika <veronka97@gmail.com>
2020-09-11 23:27:17 +0300

    Updates

---
+++ precache-manifest.a1bcaa3303c221f39e97bf8efff3245e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68afea535d4a566a9de044eb4154c7b6",
+    "url": "/github.iogit%20init/react-hw/index.html"
+  },
+  {
+    "revision": "49494e3e329422fd7ec1",
+    "url": "/github.iogit%20init/react-hw/static/css/main.f6672412.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/github.iogit%20init/react-hw/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.iogit%20init/react-hw/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49494e3e329422fd7ec1",
+    "url": "/github.iogit%20init/react-hw/static/js/main.ee50fb36.chunk.js"
+  },
+  {
+    "revision": "30ac1e8074f8df3eafd6",
+    "url": "/github.iogit%20init/react-hw/static/js/runtime-main.a958702f.js"
+  },
+  {
+    "revision": "e76a74fd4e332c9638337854c58332e6",
+    "url": "/github.iogit%20init/react-hw/static/media/space.e76a74fd.jpg"
+  }
+]);
