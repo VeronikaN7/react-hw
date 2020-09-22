5c6333817c62bb3fd16f4bf93f4dca6268968d93
Veronika <veronka97@gmail.com>
2020-09-22 22:44:09 +0300

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Post from './component/post';
 
 const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
-
 const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
 
 function App() {
--- src/component/post.js
+++ src/component/post.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Img from 'img';
 
 const Post = (props) => {
   return (
