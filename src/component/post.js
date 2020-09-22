cf37ecef0862138a117f5867c24e60e95e239d60
Veronika <veronka97@gmail.com>
2020-09-22 23:23:00 +0300

    a

--- src/component/post.js
+++ src/component/post.js
@@ -19,18 +19,18 @@ const Post = (props) => {
       <div>
         <img className="post-photo" src={props.image} alt="main-post"/>
       </div>
-      <div className="add-functions">
-        {/*<div className="item-function"> 
+      {/* <div className="add-functions">
+        <div className="item-function"> 
           <img src={require("/img/share.png")} className="icons" alt="comment-icon"/>
           <p>482</p>
-  </div>*/}
+  </div>
         <div className="item-function"><img src={require("/img/share.png")} className="icons" alt="share-icon"/>
         <p>146</p> </div>
         <div className="item-function"><img src={require("/img/like.png")} className="icons" alt="like-icon"/>
         <p>88</p></div>
         <div className="item-function"><img src={require("/img/download.png")} className="icons" alt="download-icon"/>
         </div>
-      </div>
+      </div> */}
     </div>
 
 </div>
